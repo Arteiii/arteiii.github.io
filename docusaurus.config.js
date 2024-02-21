cbc95196af334056db3430064ac55ff14971aaa2
Ben <ben.arteii@proton.me>
2024-02-21 13:25:12 +0100

    Add Algolia search integration to the website configuration. Include application ID, API key, index name, and search settings.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -143,6 +143,34 @@ module.exports = {
       // I built this website for my own personal use, but you are free to use it so long as you credit me. You can do so by linking back to evantay.com :)
       copyright: `Arteii • <a href="https://github.com/arteiii/arteiii.github.io/commits/main">Updated ${new Date().toLocaleDateString()}</a>`,
     },
+
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'D3ST694UAZ',
+
+      // Public API key: it is safe to commit it
+      apiKey: 'd358a9d8d689d82a8e64b1dcf4f99b33',
+
+      indexName: 'arteiiiio',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      // externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      // replaceSearchResultPathname: {
+      //   from: '/docs/', // or as RegExp: /\/docs\//
+      //   to: '/',
+      // },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: 'search',
+    },
   },
 
   presets: [
